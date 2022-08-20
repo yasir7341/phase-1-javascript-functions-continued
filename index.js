587fe1376b34e3240739243c51f9419525da37a1
yasir7341 <yasirabdulrahman75@gmail.com>
2022-08-20 13:28:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(){
+    console.log("This Saturday I want to roller-skate.");
+}
+function mondayWork(){
+    console.log("This Monday I will go to the office");
+}
+function wrapAdjective(name) {
+    const part1 = "you";
+    return function () {
+      const part2 = "are";
+      return function () {
+        console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+      };
+    };
+  }
+
+  wrapAdjective("100%")("a dedicated programmer")();
